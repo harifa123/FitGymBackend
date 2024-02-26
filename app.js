32c7c6de6b24f465230d82340811ceaa339743cf
Melvinpml <melvinpoulose06@gmail.com>
2024-02-26 13:54:39 +0530

    successfully added packages with common db

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb+srv://melvinpoulose06:melvinpml4151@cluster0.yshbagz.mongodb.net/fitGymDb?retryWrites=true&w=majority",{useNewUrlParser:true})
+mongoose.connect("mongodb+srv://harifa123:harifa123@cluster0.j6vqcp5.mongodb.net/gymDb?retryWrites=true&w=majority",{useNewUrlParser:true})
 
 app.use("/api/fitgym",packageRouter)
 
