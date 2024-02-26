49c60fa514d691af7dc41484ffb52b63a1d32544
MhmdKhaleel <khaleel3936@gmail.com>
2024-02-26 14:09:05 +0530

    Add Trainer Complete

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-hello
+const express=require("express")
+const cors=require("cors")
+const mongoose=require("mongoose")
+const gymrouter=require("./controllers/AddTrainer")
+
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://harifa123:harifa123@cluster0.j6vqcp5.mongodb.net/gymDb?retryWrites=true&w=majority&appName=Cluster0",
+{useNewurlParser:true}
+)
+
+app.use('/api/gym',gymrouter)
+
+app.listen(1001,()=>{
+    console.log("server running")
+})
