bdc72d756359f22f51299ca1c314715dea722364
Naslanas <naslava00@gmail.com>
2024-02-26 16:56:55 +0530

    add member completed

---
+++ Models/MemberModel.js
@@ -0,0 +1,43 @@
+const mongoose=require("mongoose")
+const memberSchema=new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        place:{
+            type:String,
+            required:true
+        },
+        age:{
+            type:String,
+            required:true
+        },
+        height:{
+            type:String,
+            required:true
+        },
+        weight:{
+            type:String,
+            required:true
+        },
+        bloodGroup:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        },
+        registerDate:{
+            type:Date,
+            default:Date.now
+        }
+    }
+)
+
+module.exports=mongoose.model("member",memberSchema)
