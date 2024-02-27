ae68702bd556635f8931c18c293da52bcfbc66eb
AbhinandhV <abhinandh2255@gmail.com>
2024-02-27 10:59:16 +0530

    completed search user

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ app.use(express.json())
 app.use(cors())
 
 
-// mongoose.connect("mongodb+srv://harifa123:harifa123@cluster0.j6vqcp5.mongodb.net/gymDb?retryWrites=true&w=majority")
-mongoose.connect("mongodb+srv://abhinandh:jazz9333@cluster0.ubk8s.mongodb.net/gymdb?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://harifa123:harifa123@cluster0.j6vqcp5.mongodb.net/gymDb?retryWrites=true&w=majority",
 {
     useNewUrlParser:true
 })
