acf618b144eaebbe9a340df15987c958075e41b9
Melvinpml <melvinpoulose06@gmail.com>
2024-02-27 21:42:09 +0530

    sigin trainer completed

--- controllers/Trainer.js
+++ controllers/Trainer.js
@@ -22,7 +22,28 @@ router.post('/addtrainer', async (req, res) => {
     }
 })
 
+router.post("/signintrainer", async(req,res)=>{
+    let input = req.body
+    let emailid = req.body.emailid
+    let data = await trainerModel.findOne({"emailid":emailid})
+    if(!data)
+    {
+        return res.json({status:"Incorrect email id"})
+    }
+    console.log(data)
+    let dbPasswordTrainer = data.password
+    let inputPasswordTrainer = req.body.password
+    console.log(dbPasswordTrainer)
+    console.log(inputPasswordTrainer)
 
+    const match = await bcrypt.compare(inputPasswordTrainer,dbPasswordTrainer)
+    if(!match)
+    {
+        return res.json({status:"Incorrect password"})
+    }
+
+    res.json({status:"success", "trainer data":data})
+})
 
 router.get("/viewtrainers", async(req,res)=>{
     let trainers = await trainerModel.find()
